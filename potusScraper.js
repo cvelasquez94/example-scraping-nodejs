7c7cab93f12a82193e61f5d61df3a177ebf6137b
carlos velasquez <cvelasquezr@teco.com.ar>
2019-09-30 09:43:41 -0300

    feat(detail): detail status

--- potusScraper.js
+++ potusScraper.js
@@ -3,25 +3,56 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const potusParse = require('./potusParse');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const url = 'https://www.buenosaires.gob.ar/subte';
 
 rp(url)
   .then((html) => {
     //success!
     const wikiUrls = [];
-    for (let i = 0; i < 45; i++) {
-      wikiUrls.push($('big > a', html)[i].attribs.href);
-    }
-    return Promise.all(
-      wikiUrls.map((url) => {
-        return potusParse(`https://en.wikipedia.org${url}`);
-      })
-    );
-  })
-  .then((presidents) => {
-    console.log(presidents);
+    // for (let i = 0; i < 45; i++) {
+    //   wikiUrls.push([i].attribs.href);
+    // }
+    
+    const content = $('div.pane-content', html)    // console.log(JSON.stringify(content.prevObject['0']), 'zzz')
+    console.log(content['1'].next.parent.children, 'zzz')
+    return content
+    //return Promise.all(
+    //  wikiUrls.map((url) => {
+    //    return potusParse(`https://en.wikipedia.org${url}`);
+    //  })
+    //);
   })
   .catch((err) => {
     //handle error
     console.log(err);
   });
+
+
+
+  //Use JSON.stringify with a custom replacer. For example:
+
+// Demo: Circular reference
+
+
+// Note: cache should not be re-used by repeated calls to JSON.stringify.
+function a (o) {
+  var cache = [];
+  JSON.stringify(o, function(key, value) {
+      if (typeof value === 'object' && value !== null) {
+          if (cache.indexOf(value) !== -1) {
+              // Duplicate reference found
+              try {
+                  // If this value does not reference a parent it can be deduped
+                  return JSON.parse(JSON.stringify(value));
+              } catch (error) {
+                  // discard key if value cannot be deduped
+                  return;
+              }
+          }
+          // Store value in our collection
+          cache.push(value);
+      }
+      return value;
+  });
+  cache = null
+}
--- renderingJsPage.js
+++ renderingJsPage.js
@@ -1,7 +1,8 @@
 const rp = require('request-promise');
-const url = 'https://pokemondb.net/pokedex/national';
+const url = 'https://www.buenosaires.gob.ar/subte';
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+const $ = require('cheerio');
 
 puppeteer
     .launch()
@@ -12,12 +13,31 @@ puppeteer
         return page.goto(url)
             .then(() => page.content());
     })
-    .then(html => createPageHatml(html))
+    .then(html => {
+        //createPageHatml(html)
+        getStatusSubte(html).then((data) => console.log(data))
+    })
     .catch(err => console.log(err));
 
 const createPageHatml = (html) => {
     fs.writeFile('index.html', html, (err) => {
         if(err) throw err;
         console.log('ook!')
+    })
+}
+
+
+const getStatusSubte = html => {
+    return new Promise((resolve, reject) => {
+        const subtes = Array('A', 'B', 'C', 'D', 'E', 'H')
+        const data = subtes.map(linea => {
+            const content = $(`#linea${linea}`, html)
+            let data = content['0'].children[1].children[3].children[1].children[0].data        
+            return ({
+                linea: linea,
+                status: data || 'Servicio con demora'
+            })
+        })
+        resolve(data)
     })
 }
